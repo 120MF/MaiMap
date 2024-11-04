1ef6c3ed09c2468e69557d822ad7cc9a0f2457fa
MoonFeather <moonbite233@gmail.com>
2024-11-04 12:03:52 +0800

    add about model and version info

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|card|link|select|slider|ripple|spinner|listbox|divider|popover|scroll-shadow).js",
+    "./node_modules/@nextui-org/theme/dist/components/(button|card|link|modal|select|slider|ripple|spinner|listbox|divider|popover|scroll-shadow).js",
   ],
   theme: {
     extend: {
