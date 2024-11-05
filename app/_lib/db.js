97e8590679985594e974b6cfdba832d36c4b84d9
MoonFeather <moonbite233@gmail.com>
2024-11-05 11:09:42 +0800

    update database information

--- app/_lib/db.js
+++ app/_lib/db.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   connectionLimit: 10, // 连接池中最大连接数
-  host: "maimap-arcades-mysql.ns-lpkl7gos.svc",
+  host: "maimap-mysql.mfnest.tech",
   user: "root",
-  password: "rhdb4cml",
+  password: "yelsjdhl",
   database: "maimap",
 });
 module.exports = pool;
