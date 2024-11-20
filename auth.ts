b4f8737d8ca6ac8f07276345906617b42b8a9166
MoonFeather <moonbite233@gmail.com>
2024-11-20 09:01:06 +0800

    add nodemailer

--- auth.ts
+++ auth.ts
@@ -1,8 +1,16 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Nodemailer from "next-auth/providers/nodemailer";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 
 import client from "./lib/db";
+import {
+  EMAIL_FROM,
+  EMAIL_SERVER_HOST,
+  EMAIL_SERVER_PASSWORD,
+  EMAIL_SERVER_PORT,
+  EMAIL_SERVER_USER,
+} from "@/lib/smtp";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: MongoDBAdapter(client),
@@ -10,6 +18,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GitHub({
       clientId: process.env.NEXT_PUBLIC_AUTH_GITHUB_ID,
       clientSecret: process.env.NEXT_PUBLIC_AUTH_GITHUB_SECRET,
+    }),
+    Nodemailer({
+      server: {
+        host: EMAIL_SERVER_HOST,
+        port: EMAIL_SERVER_PORT,
+        auth: {
+          user: EMAIL_SERVER_USER,
+          pass: EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: EMAIL_FROM,
     }),
   ],
   secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
