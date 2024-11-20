8073b6fdf94759cb4f9d7e3bf323d3550836faae
MoonFeather <moonbite233@gmail.com>
2024-11-20 22:21:15 +0800

    add password signin

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { signIn, auth, providerMap } from "@/auth";
 
 import SignInForm from "@/components/SignInForm";
 
@@ -15,48 +14,8 @@ export default function SignInPage(props: {
         src="/error-salt.png"
         width={250}
       />
-      <SignInForm />
+      {/*@ts-ignore*/}
+      <SignInForm props={props} />
     </div>
-    // <div className="flex flex-col gap-2">
-    //   <form
-    //     action={async (formData) => {
-    //       "use server";
-    //       try {
-    //         await signIn("credentials", formData);
-    //       } catch (error) {
-    //         throw error;
-    //       }
-    //     }}
-    //   >
-    //     <label htmlFor="email">
-    //       Email
-    //       <input id="email" name="email" />
-    //     </label>
-    //     <label htmlFor="password">
-    //       Password
-    //       <input id="password" name="password" />
-    //     </label>
-    //     <input type="submit" value="Sign In" />
-    //   </form>
-    //   {Object.values(providerMap).map((provider) => (
-    //     <form
-    //       key={provider.name}
-    //       action={async () => {
-    //         "use server";
-    //         try {
-    //           await signIn(provider.id, {
-    //             redirectTo: props.searchParams?.callbackUrl ?? "",
-    //           });
-    //         } catch (error) {
-    //           throw error;
-    //         }
-    //       }}
-    //     >
-    //       <button type="submit">
-    //         <span>Sign in with {provider.name}</span>
-    //       </button>
-    //     </form>
-    //   ))}
-    // </div>
   );
 }
--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -5,13 +5,54 @@ import { useState } from "react";
 import { Divider } from "@nextui-org/divider";
 import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { signIn } from "next-auth/react";
 
 import IconGithub from "@/components/icons/IconGithub";
 import IconOsu from "@/components/icons/IconOsu";
 import Icon388Mail from "@/components/icons/Icon388Mail";
+import IconKey from "@/components/icons/IconKey";
 
-function SignInForm() {
+interface OneClickFormInput {
+  email: string;
+}
+
+interface PasswordFormInput {
+  emailOrName: string;
+  password: string;
+}
+
+function SignInForm(props: {
+  searchParams: { callbackUrl: string | undefined };
+}) {
   const [selectedKey, setSelectedKey] = useState<string>("one-click");
+  const [loadingButton, setLoadingButton] = useState<string>("null");
+  const {
+    register: oneClickRegister,
+    handleSubmit: oneClickHandleSubmit,
+    formState: { errors: oneClickErrors },
+  } = useForm<OneClickFormInput>();
+  const {
+    register: passwordRegister,
+    handleSubmit: passwordHandleSubmit,
+    formState: { errors: passwordErrors },
+  } = useForm<PasswordFormInput>();
+  const onOneClickSubmit: SubmitHandler<OneClickFormInput> = async (data) => {
+    setLoadingButton("email");
+    try {
+      await signIn("nodemailer", {
+        email: data.email,
+        redirectTo: props.searchParams?.callbackUrl ?? "",
+      });
+    } catch (error) {
+      throw error;
+    }
+  };
+  const onPasswordClickSubmit: SubmitHandler<PasswordFormInput> = async (
+    data,
+  ) => {
+    console.log(data);
+  };
 
   return (
     <Card className="w-[80%]">
@@ -29,37 +70,122 @@ function SignInForm() {
       </CardHeader>
       <CardBody>
         {selectedKey === "one-click" ? (
-          <div className="flex flex-col gap-3">
-            <Divider className="-mt-2" />
-            <Button
-              fullWidth
-              className="bg-gray-700 text-gray-50"
-              endContent={<IconGithub />}
-              size="lg"
-            >
-              使用Github登录
-            </Button>
-            <Button
-              fullWidth
-              className="bg-pink-400 text-gray-50"
-              endContent={<IconOsu />}
-              size="lg"
-            >
-              使用Osu!登录
-            </Button>
-            <Divider />
-            <Input label="电子邮箱" type="email" />
+          <>
+            <div className="flex flex-col gap-3">
+              <Divider className="-mt-2" />
+              <Button
+                fullWidth
+                className="bg-gray-700 text-gray-50"
+                endContent={<IconGithub />}
+                isLoading={loadingButton === "github"}
+                size="lg"
+                onPress={() => {
+                  try {
+                    setLoadingButton("github");
+                    signIn("github", {
+                      redirectTo: props.searchParams?.callbackUrl ?? "",
+                    });
+                  } catch (error) {
+                    throw error;
+                  }
+                }}
+              >
+                使用Github登录
+              </Button>
+              <Button
+                fullWidth
+                className="bg-pink-400 text-gray-50"
+                endContent={<IconOsu />}
+                isLoading={loadingButton === "osu"}
+                size="lg"
+                onPress={() => {
+                  try {
+                    setLoadingButton("osu");
+                    signIn("github", {
+                      redirectTo: props.searchParams?.callbackUrl ?? "",
+                    });
+                  } catch (error) {
+                    throw error;
+                  }
+                }}
+              >
+                使用Osu!登录
+              </Button>
+              <Divider />
+              <form
+                className="flex flex-col gap-2"
+                onSubmit={oneClickHandleSubmit(onOneClickSubmit)}
+              >
+                <Input
+                  {...oneClickRegister("email", {
+                    required: true,
+                    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i,
+                  })}
+                  label="电子邮箱"
+                  type="email"
+                />
+                {oneClickErrors?.email?.type === "required" && (
+                  <p className="text-red-400 text-xs pl-2">请输入电子邮箱</p>
+                )}
+                {oneClickErrors?.email?.type === "pattern" && (
+                  <p className="text-red-400 text-xs pl-2">
+                    请输入正确的电子邮箱
+                  </p>
+                )}
+                <Button
+                  fullWidth
+                  color="primary"
+                  endContent={<Icon388Mail />}
+                  isLoading={loadingButton === "email"}
+                  size="lg"
+                  type="submit"
+                >
+                  使用邮箱注册/登录
+                </Button>
+              </form>
+            </div>
+            <CardFooter className="pb-1">
+              <p className="text-sm opacity-40 text-center w-full">
+                未注册的账户将被自动注册
+              </p>
+            </CardFooter>
+          </>
+        ) : (
+          <form
+            className="flex flex-col gap-2"
+            onSubmit={passwordHandleSubmit(onPasswordClickSubmit)}
+          >
+            <Input
+              {...passwordRegister("emailOrName", {
+                required: true,
+              })}
+              label="电子邮箱或用户名"
+            />
+            {passwordErrors?.emailOrName?.type === "required" && (
+              <p className="text-red-400 text-xs pl-2">
+                请输入电子邮箱或用户名
+              </p>
+            )}
+            <Input
+              {...passwordRegister("password", {
+                required: true,
+              })}
+              label="密码"
+              type="password"
+            />
+            {passwordErrors?.password?.type === "required" && (
+              <p className="text-red-400 text-xs pl-2">请输入密码</p>
+            )}
             <Button
               fullWidth
               color="primary"
-              endContent={<Icon388Mail />}
+              endContent={<IconKey />}
               size="lg"
+              type="submit"
             >
-              使用邮箱注册/登录
+              使用账号/密码登录
             </Button>
-          </div>
-        ) : (
-          <div>账密登录</div>
+          </form>
         )}
       </CardBody>
     </Card>
