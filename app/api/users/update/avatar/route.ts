05a05b65a88e20a1ddce243212e193e95cfbcbd5
MoonFeather <moonbite233@gmail.com>
2024-12-01 15:01:56 +0800

    add GridFS uploader

---
+++ app/api/users/update/avatar/route.ts
@@ -0,0 +1,63 @@
+import type { NextRequest } from "next/server";
+
+import { Readable } from "node:stream";
+
+import { GridFSBucket, ObjectId } from "mongodb";
+
+import client from "@/lib/db";
+import { auth } from "@/auth";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(request: NextRequest) {
+  const session = await auth();
+
+  if (!session) {
+    return new Response(JSON.stringify({ message: "You must login first." }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  const data = await request.formData();
+  const image = data.get("image") as File;
+  const arrayBuffer = await image.arrayBuffer();
+
+  const readableStream = new Readable();
+
+  readableStream._read = () => {};
+  readableStream.push(Buffer.from(arrayBuffer));
+  readableStream.push(null);
+
+  try {
+    await client.connect();
+    const db = client.db("maimap");
+    const bucket = new GridFSBucket(db, { bucketName: "avatars" });
+
+    const uploadStream = bucket.openUploadStream(image.name, {
+      metadata: { user: new ObjectId(session.user.id) },
+    });
+
+    readableStream.pipe(uploadStream);
+
+    return new Response(
+      JSON.stringify({ message: "File uploaded successfully." }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ message: "Error connecting to database.", error }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+}
