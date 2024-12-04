6c6eac3e4be80f5c6ba01c95b487244dc5a3e95b
MoonFeather <moonbite233@gmail.com>
2024-12-04 22:22:12 +0800

    add salt picture in login page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import SignInForm from "@/components/SignInForm";
 
 export default function SignInPage(props: {
@@ -7,13 +5,6 @@ export default function SignInPage(props: {
 }) {
   return (
     <div className="h-screen w-full flex flex-col items-center backdrop-blur">
-      <Image
-        alt="saltislookingatyou"
-        className="-mb-5"
-        height={250}
-        src="/error-salt.png"
-        width={250}
-      />
       {/*@ts-ignore*/}
       <SignInForm props={props} />
     </div>
--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -11,6 +11,7 @@ import { toast } from "react-toastify";
 import { ToastProps } from "next/dist/client/components/react-dev-overlay/internal/components/Toast/Toast";
 import { useTheme } from "next-themes";
 import { useSearchParams } from "next/navigation";
+import Image from "next/image";
 
 import IconGithub from "@/components/icons/IconGithub";
 import IconOsu from "@/components/icons/IconOsu";
@@ -82,143 +83,155 @@ function SignInForm(props: {
       });
     }
   };
+  const [isInputPassword, setIsInputPassword] = useState(false);
 
   return (
-    <Card className="w-[80%]">
-      <CardHeader>
-        <Tabs
-          fullWidth
-          aria-label="options"
-          selectedKey={selectedKey}
-          // @ts-ignore
-          onSelectionChange={setSelectedKey}
-        >
-          <Tab key="one-click" title="一键登录" />
-          <Tab key="password" title="账密登录" />
-        </Tabs>
-      </CardHeader>
-      <CardBody>
-        {selectedKey === "one-click" ? (
-          <>
-            <div className="flex flex-col gap-3">
-              <Divider className="-mt-2" />
-              <Button
-                fullWidth
-                className="bg-gray-700 text-gray-50"
-                endContent={<IconGithub />}
-                isLoading={loadingButton === "github"}
-                size="lg"
-                onPress={() => {
-                  try {
-                    setLoadingButton("github");
-                    signIn("github", {
-                      redirectTo: props.searchParams?.callbackUrl ?? "",
-                    });
-                  } catch (error) {
-                    throw error;
-                  }
-                }}
-              >
-                使用Github登录
-              </Button>
-              <Button
-                fullWidth
-                className="bg-pink-400 text-gray-50"
-                endContent={<IconOsu />}
-                isLoading={loadingButton === "osu"}
-                size="lg"
-                onPress={() => {
-                  try {
-                    setLoadingButton("osu");
-                    signIn("osu", {
-                      redirectTo: props.searchParams?.callbackUrl ?? "",
-                    });
-                  } catch (error) {
-                    throw error;
-                  }
-                }}
-              >
-                使用Osu!登录
-              </Button>
-              <Divider />
-              <form
-                className="flex flex-col gap-2"
-                onSubmit={oneClickHandleSubmit(onOneClickSubmit)}
-              >
-                <Input
-                  {...oneClickRegister("email", {
-                    required: true,
-                    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i,
-                  })}
-                  label="电子邮箱"
-                  type="email"
-                />
-                {oneClickErrors?.email?.type === "required" && (
-                  <p className="text-red-400 text-xs pl-2">请输入电子邮箱</p>
-                )}
-                {oneClickErrors?.email?.type === "pattern" && (
-                  <p className="text-red-400 text-xs pl-2">
-                    请输入正确的电子邮箱
-                  </p>
-                )}
+    <>
+      <Image
+        alt="saltislookingatyou"
+        className="-mb-5"
+        height={250}
+        src={`${isInputPassword ? "/password-salt.png" : "/user-salt.png"}`}
+        width={250}
+      />
+      <Card className="w-[80%]">
+        <CardHeader>
+          <Tabs
+            fullWidth
+            aria-label="options"
+            selectedKey={selectedKey}
+            // @ts-ignore
+            onSelectionChange={setSelectedKey}
+          >
+            <Tab key="one-click" title="一键登录" />
+            <Tab key="password" title="账密登录" />
+          </Tabs>
+        </CardHeader>
+        <CardBody>
+          {selectedKey === "one-click" ? (
+            <>
+              <div className="flex flex-col gap-3">
+                <Divider className="-mt-2" />
                 <Button
                   fullWidth
-                  color="primary"
-                  endContent={<Icon388Mail />}
-                  isLoading={loadingButton === "email"}
+                  className="bg-gray-700 text-gray-50"
+                  endContent={<IconGithub />}
+                  isLoading={loadingButton === "github"}
                   size="lg"
-                  type="submit"
+                  onPress={() => {
+                    try {
+                      setLoadingButton("github");
+                      signIn("github", {
+                        redirectTo: props.searchParams?.callbackUrl ?? "",
+                      });
+                    } catch (error) {
+                      throw error;
+                    }
+                  }}
                 >
-                  使用邮箱注册/登录
+                  使用Github登录
                 </Button>
-              </form>
-            </div>
-            <CardFooter className="pb-1">
-              <p className="text-sm opacity-40 text-center w-full">
-                未注册的账户将被自动注册
-              </p>
-            </CardFooter>
-          </>
-        ) : (
-          <form
-            className="flex flex-col gap-2"
-            onSubmit={passwordHandleSubmit(onPasswordClickSubmit)}
-          >
-            <Input
-              {...passwordRegister("emailOrName", {
-                required: true,
-              })}
-              label="电子邮箱或用户名"
-            />
-            {passwordErrors?.emailOrName?.type === "required" && (
-              <p className="text-red-400 text-xs pl-2">
-                请输入电子邮箱或用户名
-              </p>
-            )}
-            <Input
-              {...passwordRegister("password", {
-                required: true,
-              })}
-              label="密码"
-              type="password"
-            />
-            {passwordErrors?.password?.type === "required" && (
-              <p className="text-red-400 text-xs pl-2">请输入密码</p>
-            )}
-            <Button
-              fullWidth
-              color="primary"
-              endContent={<IconKey />}
-              isLoading={loadingButton === "password"}
-              size="lg"
-              type="submit"
+                <Button
+                  fullWidth
+                  className="bg-pink-400 text-gray-50"
+                  endContent={<IconOsu />}
+                  isLoading={loadingButton === "osu"}
+                  size="lg"
+                  onPress={() => {
+                    try {
+                      setLoadingButton("osu");
+                      signIn("osu", {
+                        redirectTo: props.searchParams?.callbackUrl ?? "",
+                      });
+                    } catch (error) {
+                      throw error;
+                    }
+                  }}
+                >
+                  使用Osu!登录
+                </Button>
+                <Divider />
+                <form
+                  className="flex flex-col gap-2"
+                  onSubmit={oneClickHandleSubmit(onOneClickSubmit)}
+                >
+                  <Input
+                    {...oneClickRegister("email", {
+                      required: true,
+                      pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i,
+                    })}
+                    label="电子邮箱"
+                    type="email"
+                  />
+                  {oneClickErrors?.email?.type === "required" && (
+                    <p className="text-red-400 text-xs pl-2">请输入电子邮箱</p>
+                  )}
+                  {oneClickErrors?.email?.type === "pattern" && (
+                    <p className="text-red-400 text-xs pl-2">
+                      请输入正确的电子邮箱
+                    </p>
+                  )}
+                  <Button
+                    fullWidth
+                    color="primary"
+                    endContent={<Icon388Mail />}
+                    isLoading={loadingButton === "email"}
+                    size="lg"
+                    type="submit"
+                  >
+                    使用邮箱注册/登录
+                  </Button>
+                </form>
+              </div>
+              <CardFooter className="pb-1">
+                <p className="text-sm opacity-40 text-center w-full">
+                  未注册的账户将被自动注册
+                </p>
+              </CardFooter>
+            </>
+          ) : (
+            <form
+              className="flex flex-col gap-2"
+              onSubmit={passwordHandleSubmit(onPasswordClickSubmit)}
             >
-              使用账号/密码登录
-            </Button>
-          </form>
-        )}
-      </CardBody>
-    </Card>
+              <Input
+                {...passwordRegister("emailOrName", {
+                  required: true,
+                })}
+                label="电子邮箱或用户名"
+              />
+              {passwordErrors?.emailOrName?.type === "required" && (
+                <p className="text-red-400 text-xs pl-2">
+                  请输入电子邮箱或用户名
+                </p>
+              )}
+              <Input
+                {...passwordRegister("password", {
+                  required: true,
+                })}
+                label="密码"
+                type="password"
+                onBlur={() => setIsInputPassword(false)}
+                onFocus={() => setIsInputPassword(true)}
+              />
+              {passwordErrors?.password?.type === "required" && (
+                <p className="text-red-400 text-xs pl-2">请输入密码</p>
+              )}
+              <Button
+                fullWidth
+                color="primary"
+                endContent={<IconKey />}
+                isLoading={loadingButton === "password"}
+                size="lg"
+                type="submit"
+              >
+                使用账号/密码登录
+              </Button>
+            </form>
+          )}
+        </CardBody>
+      </Card>
+    </>
   );
 }
 
