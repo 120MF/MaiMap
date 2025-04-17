1874cd5ef07ac00e44f15a75d507946b4a364f09
moonfeather <moonbite233@gmail.com>
2025-04-17 15:21:01 +0800

    add circle

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from '@/stores/useTheme.tsx';
 import React, { useRef, useState, useEffect } from 'react';
-import { TMap } from 'tlbs-map-react';
+import { BaseMap, MultiCircle } from 'tlbs-map-react';
 import { MultiMarker } from 'tlbs-map-react';
 
 const key = process.env.QMAP_KEY;
@@ -14,39 +14,58 @@ const styles = {
     height: 30,
     anchor: { x: 10, y: 30 },
   },
+  circleStyle: {
+    color: 'rgba(41,91,255,0.16)',
+    showBorder: true,
+    borderColor: 'rgba(41,91,255,1)',
+    borderWidth: 2,
+  },
 };
 
-const geometries = [
+const markerGeometry = [
   {
     styleId: 'multiMarkerStyle',
     position: { lat: 40.0404, lng: 116.2735 },
   },
 ];
 
+const circleGeometry = [
+  {
+    styleId: 'circleStyle',
+    center: { lat: 40.0404, lng: 116.2735 },
+    radius: 5000,
+  },
+];
+
 function MapContainer() {
   const mapRef = useRef(null);
   const markerRef = useRef(null);
+  const layerRef = useRef(null);
   const { theme } = useTheme();
-  const [key, setKey] = useState(0);
-
+  const [key_, setKey_] = useState(0);
   const mapStyleId = theme === 'dark' ? DARK_STYLE : LIGHT_STYLE;
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <theme变化时刷新Map组件>
   useEffect(() => {
-    setKey((prevKey) => prevKey + 1);
+    setKey_((prevKey) => prevKey + 1);
   }, [theme]);
 
   return (
-    <TMap
-      key={key} // 添加key属性实现强制重新渲染
+    <BaseMap
+      key={key_} // 添加key属性实现强制重新渲染
       ref={mapRef}
       apiKey={`${key}`}
       style={{ height: '90svh', width: '100%' }}
-      className="z-10"
-      options={{ mapStyleId: mapStyleId }}
+      className="z-20"
+      options={{
+        center: { lat: 40.0404, lng: 116.2735 },
+        mapStyleId: mapStyleId,
+        type: 'vector',
+        features: ['base', 'building3d'],
+      }}
     >
-      <MultiMarker ref={markerRef} styles={styles} geometries={geometries} />
-    </TMap>
+      <MultiCircle ref={layerRef} geometries={circleGeometry} styles={styles} />
+    </BaseMap>
   );
 }
 
